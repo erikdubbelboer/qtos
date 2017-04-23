10a8e9ac51d9eab29e34867219c8cb3b6b307cad
Erik Dubbelboer <erik@dubbelboer.com>
2017-04-23 12:21:38 +0800

    Support many more format and values

--- qtos.go
+++ qtos.go
@@ -1,7 +1,7 @@
 package qtos
 
 import (
-	"errors"
+	"fmt"
 	"net/url"
 	"reflect"
 	"regexp"
@@ -10,14 +10,13 @@ import (
 )
 
 var (
-	ErrMustBeStruct  = errors.New("Must be a struct")
-	ErrMustBePointer = errors.New("v must be a pointer")
-
 	// StructTag is the struct tag key being used.
 	// Assigning to this variable is not thread safe.
 	StructTag = "query"
 
-	mapRegexp = regexp.MustCompile(`\[[a-z]+\]$`)
+	fieldRegexp  = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*`)
+	indexRegexp  = regexp.MustCompile(`^\[[0-9]+\]`)
+	mapKeyRegexp = regexp.MustCompile(`^\[[^\]]+\]`)
 )
 
 // Unmarshal parses the url values and stores the result in the value pointed to by v
@@ -27,20 +26,227 @@ func Unmarshal(values url.Values, v interface{}) error {
 	val := reflect.ValueOf(v)
 
 	if val.Kind() != reflect.Ptr || val.IsNil() {
-		return ErrMustBePointer
+		return fmt.Errorf("v must be a pointer")
 	} else {
 		typ = typ.Elem()
 		val = val.Elem()
 	}
 
-	return bindStruct(typ, val, "", values)
-}
-
-func bindStruct(typ reflect.Type, val reflect.Value, prefix string, values url.Values) error {
-	if typ.Kind() != reflect.Struct {
-		return ErrMustBeStruct
+	for name, value := range values {
+		if err := bind(typ, val, "", name, value); err != nil {
+			return err
+		}
 	}
 
+	return nil
+}
+
+// bind is called recursively while parsing the value name.
+// base contains the parsed part so far and is only used to product nice error messages.
+func bind(typ reflect.Type, val reflect.Value, base, name string, value []string) error {
+	// If the name is empty it means we should assign value to val.
+	if name == "" {
+		if len(value) > 1 {
+			return fmt.Errorf("expected only one value for %s got %v", base, value)
+		}
+		if v, err := getValue(typ, value[0]); err != nil {
+			return err
+		} else {
+			val.Set(v)
+			return nil
+		}
+	}
+
+	// We can ignore leading dots.
+	if name[0] == '.' {
+		base = base + "."
+		name = name[1:]
+	}
+
+	// Is it a struct field?
+	if field := fieldRegexp.FindString(name); field != "" {
+		if typ.Kind() != reflect.Struct {
+			return fmt.Errorf("expected a struct for %s got %v", base, typ)
+		}
+
+		i, ok := getStructField(typ, field)
+		if !ok {
+			// The struct doesn't have any field with this name.
+			return nil
+		}
+
+		return bind(typ.Field(i).Type, val.Field(i), base+name[:len(field)], name[len(field):], value)
+	}
+
+	// Is it a slice?
+	if strings.HasPrefix(name, "[]") {
+		if typ.Kind() != reflect.Slice {
+			return fmt.Errorf("expected a slice for %s got %v", base, typ)
+		}
+
+		if len(name) != 2 {
+			return fmt.Errorf("[] can only be at the end of the key")
+		}
+
+		for _, v := range value {
+			if vv, err := getValue(typ.Elem(), v); err != nil {
+				return err
+			} else {
+				val.Set(reflect.Append(val, vv))
+			}
+		}
+		return nil
+	}
+
+	// Only try this with a slice destination, otherwise it might be an integer map key.
+	if typ.Kind() == reflect.Slice {
+		// Is it a slice index?
+		if indexStr := indexRegexp.FindString(name); indexStr != "" {
+			index, _ := strconv.Atoi(indexStr[1 : len(indexStr)-1])
+
+			// Do we need to create the slice?
+			if val.IsNil() {
+				val.Set(reflect.MakeSlice(val.Type(), index+1, index+1))
+			} else if val.Len() < index+1 {
+				// The slice isn't big enough.
+				if val.Cap() < index+1 {
+					n := reflect.MakeSlice(val.Type(), index+1, index+1)
+					reflect.Copy(n, val)
+					val.Set(n)
+				} else {
+					val.SetLen(index + 1)
+				}
+			}
+
+			t := typ.Elem()
+			v := reflect.Indirect(reflect.New(t))
+
+			if err := bind(t, v, base+name[:len(indexStr)], name[len(indexStr):], value); err != nil {
+				return err
+			} else if mv, err := mergeValues(val.Index(index), v); err != nil {
+				return err
+			} else {
+				val.Index(index).Set(mv)
+				return nil
+			}
+		}
+	}
+
+	// Is it a map key?
+	if key := mapKeyRegexp.FindString(name); key != "" {
+		if typ.Kind() != reflect.Map {
+			return fmt.Errorf("expected a map for %s got %v", base, typ)
+		}
+
+		if val.IsNil() {
+			// Create the map if it doesn't exist yet.
+			val.Set(reflect.MakeMap(typ))
+		}
+
+		t := typ.Elem()
+		v := reflect.Indirect(reflect.New(t))
+
+		if err := bind(t, v, base+name[:len(key)], name[len(key):], value); err != nil {
+			return err
+		} else {
+			if k, err := getValue(typ.Key(), key[1:len(key)-1]); err != nil {
+				return err
+			} else if mv, err := mergeValues(val.MapIndex(k), v); err != nil {
+				return err
+			} else {
+				val.SetMapIndex(k, mv)
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("unknown format %s in %s", name, base+name)
+}
+
+func mergeValues(a, b reflect.Value) (reflect.Value, error) {
+	if !a.IsValid() {
+		return b, nil
+	}
+
+	if a.Kind() != b.Kind() {
+		return a, fmt.Errorf("can not merge %v and %v", a.Type(), b.Type())
+	}
+
+	switch a.Kind() {
+	case reflect.Slice:
+		if a.IsNil() {
+			return b, nil
+		}
+
+		// Make sure a is always the longest slice.
+		if b.Len() > a.Len() {
+			a, b = b, a
+		}
+
+		for i := 0; i < b.Len(); i++ {
+			a.Index(i).Set(b.Index(i))
+		}
+
+		return a, nil
+	case reflect.Map:
+		if a.IsNil() {
+			return b, nil
+		}
+
+		for _, k := range b.MapKeys() {
+			a.SetMapIndex(k, b.MapIndex(k))
+		}
+
+		return a, nil
+	default:
+		// For most values like string we just return the second value
+		// so it will always overwrite the first.
+		return b, nil
+	}
+}
+
+// getValue returns value as a reflect.Value of type typ.
+func getValue(typ reflect.Type, value string) (reflect.Value, error) {
+	switch typ.Kind() {
+	case reflect.String:
+		return reflect.ValueOf(value), nil
+	case reflect.Int, reflect.Int32, reflect.Int64:
+		if i, err := strconv.ParseInt(value, 10, 64); err != nil {
+			return reflect.Zero(typ), err
+		} else {
+			v := reflect.Indirect(reflect.New(typ))
+			v.SetInt(i)
+			return v, nil
+		}
+	case reflect.Float32, reflect.Float64:
+		if f, err := strconv.ParseFloat(value, 64); err != nil {
+			return reflect.Zero(typ), err
+		} else {
+			v := reflect.Indirect(reflect.New(typ))
+			v.SetFloat(f)
+			return v, nil
+		}
+	case reflect.Bool:
+		if b, err := strconv.ParseBool(value); err != nil {
+			return reflect.Zero(typ), err
+		} else {
+			v := reflect.Indirect(reflect.New(typ))
+			v.SetBool(b)
+			return v, nil
+		}
+	case reflect.Interface:
+		return reflect.ValueOf(value), nil
+	default:
+		return reflect.Zero(typ), fmt.Errorf("unsupported type %v", typ)
+	}
+}
+
+// getStructField returns the field index in typ of the field with struct
+// tag name.
+func getStructField(typ reflect.Type, name string) (int, bool) {
+	// TODO: In theory we could add a global case that caches this mapping
+	// based on typ.Name(). It would require a mutex so some benchmarking
+	// is required to see if this actually improves the speed.
 	mapping := make(map[string]int)
 
 	for i := 0; i < typ.NumField(); i++ {
@@ -55,83 +261,6 @@ func bindStruct(typ reflect.Type, val reflect.Value, prefix string, values url.V
 		mapping[valueName] = i
 	}
 
-	for name, value := range values {
-		// If we are inside a stuct we should make sure we only bind values for that struct.
-		if !strings.HasPrefix(name, prefix) {
-			continue
-		}
-
-		// Trim the struct prefix.
-		name = strings.TrimPrefix(name, prefix)
-
-		// Is it a struct?
-		if strings.Count(name, ".") > 0 {
-			parts := strings.SplitN(name, ".", 2)
-			// parts[0] will be the name of the value in the current struct.
-			i, ok := mapping[parts[0]]
-			if !ok {
-				continue
-			}
-			// Recurse into the new struct with the next prefix.
-			if err := bindStruct(typ.Field(i).Type, val.Field(i), prefix+parts[0]+".", values); err != nil {
-				return err
-			}
-			continue
-		}
-
-		// Ignore any [] at the end as our values are always a slice of values anyways.
-		if strings.HasSuffix(name, "[]") {
-			name = name[:len(name)-2]
-		}
-
-		// Is it a map? If so get the key for the map.
-		key := mapRegexp.FindString(name)
-		if key != "" {
-			// Remove the map key from the name.
-			name = strings.TrimSuffix(name, key)
-			key = key[1 : len(key)-1]
-		}
-
-		i, ok := mapping[name]
-		if !ok {
-			continue
-		}
-
-		styp := typ.Field(i)
-		sval := val.Field(i)
-
-		if err := assign(styp, sval, key, value); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func assign(typ reflect.StructField, val reflect.Value, key string, value []string) error {
-	switch typ.Type.Kind() {
-	case reflect.Int, reflect.Int32, reflect.Int64:
-		if i, err := strconv.ParseInt(value[0], 10, 64); err != nil {
-			return err
-		} else {
-			val.SetInt(i)
-		}
-	case reflect.String:
-		// Just use the zero'th value only. In theory we could also return an error if there are multiple values.
-		val.SetString(value[0])
-	case reflect.Slice:
-		// TODO: right now this will only support slices of strings because
-		// value is a slice of strings. If you want to support other slices
-		// you need to look at the type of the slice value and convert value
-		// to a slice of this type.
-		val.Set(reflect.ValueOf(value))
-	case reflect.Map:
-		if val.IsNil() {
-			// Create the map if it doesn't exist yet.
-			val.Set(reflect.MakeMap(typ.Type))
-		}
-		val.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(value))
-	}
-
-	return nil
+	i, ok := mapping[name]
+	return i, ok
 }
--- qtos_test.go
+++ qtos_test.go
@@ -6,61 +6,199 @@ import (
 	"testing"
 )
 
-type (
-	Search struct {
-		Query  string                 `query:"q"`
-		Filter map[string]interface{} `query:"filter"`
-		Fields []string               `query:"fields"`
-		Paging Paging                 `query:"paging"`
-	}
-
-	Paging struct {
-		Offset int64               `query:"offset"`
-		Size   int32               `query:"size"`
-		Foo    map[string][]string `query:"foo"`
-		Nested Boo                 `query:"nested"`
-	}
-
-	Boo struct {
-		Bar int `query:"bar"`
-	}
-)
-
 func TestUnmarshal(t *testing.T) {
-	inputs := map[string]Search{
-		"q=Honda&filter[categories][]=Motor&filter[categories][]=Mobil&fields[]=foo&fields[]=bar&paging.offset=2&paging.size=100&paging.foo[bar][]=bla&paging.nested.bar=1337": Search{
-			Query: "Honda",
-			Filter: map[string]interface{}{
-				"categories": []string{"Motor", "Mobil"},
+	type subsub struct {
+		Int int `query:"int"`
+	}
+
+	type sub struct {
+		Int         int         `query:"int"`
+		Interface   interface{} `query:"interface"`
+		SubSub      subsub      `query:"subsub"`
+		SliceString []string    `query:"slicestring"`
+	}
+
+	type base struct {
+		String               string                `query:"string"`
+		Int                  int                   `query:"int"`
+		Float                float64               `query:"float"`
+		Bool                 bool                  `query:"bool"`
+		Interface            interface{}           `query:"interface"`
+		MapStringInt         map[string]int        `query:"mapstringint"`
+		MapIntString         map[int]string        `query:"mapintstring"`
+		MapIntInterface      map[int]interface{}   `query:"mapintinterface"`
+		SliceString          []string              `query:"slicestring"`
+		SliceInt             []int                 `query:"sliceint"`
+		SliceInterface       []interface{}         `query:"sliceinterface"`
+		MapStringSliceString map[string][]string   `query:"mapstringslicestring"`
+		MapIntSliceInt       map[int][]int         `query:"mapintsliceint"`
+		MapIntSliceInterface map[int][]interface{} `query:"mapintsliceinterface"`
+		Sub                  sub                   `query:"sub"`
+		MapStringSub         map[string]sub        `query:"mapstringsub"`
+		SliceMapIntInt       []map[int]int         `query:"slicemapintint"`
+	}
+
+	inputs := map[string]interface{}{
+		"string=test": base{
+			String: "test",
+		},
+		"int=2": base{
+			Int: 2,
+		},
+		"float=2.3": base{
+			Float: 2.3,
+		},
+		"bool=true": base{
+			Bool: true,
+		},
+		"interface=test": base{
+			Interface: "test",
+		},
+		"interface=2": base{
+			Interface: "2",
+		},
+		"mapstringint[test]=2": base{
+			MapStringInt: map[string]int{
+				"test": 2,
 			},
-			Fields: []string{"foo", "bar"},
-			Paging: Paging{
-				Offset: 2,
-				Size:   100,
-				Foo: map[string][]string{
-					"bar": []string{"bla"},
-				},
-				Nested: Boo{
-					Bar: 1337,
+		},
+		"mapintstring[2]=test": base{
+			MapIntString: map[int]string{
+				2: "test",
+			},
+		},
+		"mapintinterface[2]=test": base{
+			MapIntInterface: map[int]interface{}{
+				2: "test",
+			},
+		},
+		"slicestring[]=foo&slicestring[]=bar": base{
+			SliceString: []string{"foo", "bar"},
+		},
+		"slicestring[1]=foo&slicestring[0]=bar": base{
+			SliceString: []string{"bar", "foo"},
+		},
+		"sliceint[]=2&sliceint[]=3": base{
+			SliceInt: []int{2, 3},
+		},
+		"sliceinterface[]=foo&sliceinterface[]=bar": base{
+			SliceInterface: []interface{}{"foo", "bar"},
+		},
+		"mapstringslicestring[test][]=foo&mapstringslicestring[test][]=bar&mapstringslicestring[foo][0]=bar": base{
+			MapStringSliceString: map[string][]string{
+				"test": []string{"foo", "bar"},
+				"foo":  []string{"bar"},
+			},
+		},
+		"mapstringslicestring[test][1]=foo&mapstringslicestring[test][0]=bar": base{
+			MapStringSliceString: map[string][]string{
+				"test": []string{"bar", "foo"},
+			},
+		},
+		"mapintsliceint[2][]=3&mapintsliceint[4][]=5": base{
+			MapIntSliceInt: map[int][]int{
+				2: []int{3},
+				4: []int{5},
+			},
+		},
+		"mapintsliceinterface[2][]=foo&mapintsliceinterface[2][]=bar": base{
+			MapIntSliceInterface: map[int][]interface{}{
+				2: []interface{}{"foo", "bar"},
+			},
+		},
+		"sub.int=2": base{
+			Sub: sub{
+				Int: 2,
+			},
+		},
+		"sub.interface=2": base{
+			Sub: sub{
+				Interface: "2",
+			},
+		},
+		"sub.subsub.int=2": base{
+			Sub: sub{
+				SubSub: subsub{
+					Int: 2,
 				},
 			},
 		},
+		"sub.slicestring[]=foo&sub.slicestring[]=bar": base{
+			Sub: sub{
+				SliceString: []string{"foo", "bar"},
+			},
+		},
+		"mapstringsub[foo].int=2": base{
+			MapStringSub: map[string]sub{
+				"foo": sub{
+					Int: 2,
+				},
+			},
+		},
+		"slicemapintint[0][1]=2&slicemapintint[0][3]=4": base{
+			SliceMapIntInt: []map[int]int{
+				map[int]int{
+					1: 2,
+					3: 4,
+				},
+			},
+		},
+
+		// Test some none-struct values.
+		"[]=foo&[]=bar": []string{"foo", "bar"},
+		"[]=2&[]=3":     []int{2, 3},
+		"[1]=3&[0]=2":   []int{2, 3},
+		"[foo]=bar":     map[string]string{"foo": "bar"},
+		"[2]=3":         map[int]int{2: 3},
+		"[4]=5":         map[string]int{"4": 5},
 	}
 
 	for qs, expected := range inputs {
 		t.Run(qs, func(t *testing.T) {
 			values, err := url.ParseQuery(qs)
 			if err != nil {
-				panic(err)
-			}
-
-			x := Search{}
-			if err := Unmarshal(values, &x); err != nil {
 				t.Fatal(err)
 			}
 
-			if !reflect.DeepEqual(expected, x) {
+			// Make an empty value of the same type as expected.
+			x := reflect.New(reflect.TypeOf(expected))
+			if err := Unmarshal(values, x.Interface()); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(expected, reflect.Indirect(x).Interface()) {
 				t.Fatalf("expected\n%#v\ngot\n%#v", expected, x)
+			}
+		})
+	}
+}
+
+func TestError(t *testing.T) {
+	type sub struct {
+		Int int `query:"int"`
+	}
+
+	type base struct {
+		SliceSub       []sub         `query:"slicesub"`
+		SliceMapIntInt []map[int]int `query:"slicemapintint"`
+	}
+
+	inputs := map[string]interface{}{
+		"slicesub[].int=2&slicesub[].int=3":           base{},
+		"slicemapintint[][2]=3&slicemapintint[][2]=4": base{},
+	}
+
+	for qs, expected := range inputs {
+		t.Run(qs, func(t *testing.T) {
+			values, err := url.ParseQuery(qs)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Make an empty value of the same type as expected.
+			x := reflect.New(reflect.TypeOf(expected))
+			if err := Unmarshal(values, x.Interface()); err == nil {
+				t.Fatal("expected an error")
 			}
 		})
 	}
